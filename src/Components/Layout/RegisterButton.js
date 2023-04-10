0f3f9082f2586d54a3d95593c45118e50a65364b
Konrad <konrad.gruca90@gmail.com>
2023-04-10 09:21:21 +0200

    dropdown

--- src/Components/Layout/RegisterButton.js
+++ src/Components/Layout/RegisterButton.js
@@ -1,8 +1,27 @@
 import React, { useState } from "react";
 import { Button, Menu, MenuItem } from "@material-ui/core";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import { makeStyles } from "@material-ui/core/styles";
+import { DeveloperIcon, RecruiterIcon } from "../../Utils/devIcons";
+
+const useStyles = makeStyles(() => ({
+  menuItem: {
+    display: "flex",
+    alignItems: "center",
+    height: "80px",
+    width: "265px",
+    flexDirection: "row",
+    gap: "50px",
+    border: "0.5px solid",
+    "&: hover": {
+      backgroundColor: "#f3f6f8",
+    },
+  },
+}));
+
 function RegisterButton({ handleClick }) {
   const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useStyles();
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -20,6 +39,7 @@ function RegisterButton({ handleClick }) {
           color: "white",
           borderRadius: "50px",
           display: "flex",
+          gap: "10px",
           flexDirection: "row",
         }}
         onClick={handleMenuOpen}
@@ -31,13 +51,25 @@ function RegisterButton({ handleClick }) {
         anchorEl={anchorEl}
         open={Boolean(anchorEl)}
         onClose={handleMenuClose}
-        style={{ marginTop: "50px" }}
+        style={{ marginTop: "50px", height: "285px", width: "385px" }}
       >
-        <MenuItem onClick={() => handleClick("/applicantsignup")}>
-          As Applicant
+        <MenuItem
+          className={classes.menuItem}
+          onClick={() => handleClick("/applicantsignup")}
+        >
+          <DeveloperIcon
+            style={{ backgroundColor: "#fdf8fb", color: "#ed629e" }}
+          />
+          <span> As Applicant</span>
         </MenuItem>
-        <MenuItem onClick={() => handleClick("/recruitersignup")}>
-          As Recruiter
+        <MenuItem
+          className={classes.menuItem}
+          onClick={() => handleClick("/recruitersignup")}
+        >
+          <RecruiterIcon
+            style={{ backgroundColor: "#f3e5f5", color: "#ab47bc" }}
+          />
+          <span> As Recruiter</span>
         </MenuItem>
       </Menu>
     </>
--- src/Utils/devIcons.js
+++ src/Utils/devIcons.js
@@ -1,4 +1,6 @@
-import { SvgIcon } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SvgIcon from "@material-ui/core/SvgIcon";
 import "devicon/devicon.min.css";
 
 export const devIcons = [
@@ -38,3 +40,32 @@ export const BuildingIcon = ({ color }) => {
     </SvgIcon>
   );
 };
+
+const useStyles = makeStyles(() => ({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+}));
+
+export function RecruiterIcon(props) {
+  const classes = useStyles();
+
+  return (
+    <SvgIcon viewBox="0 0 24 24" className={classes.icon} {...props}>
+      <path d="M14 6V4h-4v2h4zM4 8v11h16V8H4zm16-2c1.11 0 2 .89 2 2v11c0 1.11-.89 2-2 2H4c-1.11 0-2-.89-2-2l.01-11c0-1.11.88-2 1.99-2h4V4c0-1.11.89-2 2-2h4c1.11 0 2 .89 2 2v2h4z" />
+    </SvgIcon>
+  );
+}
+
+export function DeveloperIcon(props) {
+  const classes = useStyles();
+
+  return (
+    <SvgIcon viewBox="0 0 24 24" className={classes.icon} {...props}>
+      <path d="M10.25 13c0 .69-.56 1.25-1.25 1.25S7.75 13.69 7.75 13s.56-1.25 1.25-1.25 1.25.56 1.25 1.25zM15 11.75c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zm7 .25c0 5.52-4.48 10-10 10S2 17.52 2 12 6.48 2 12 2s10 4.48 10 10zM10.66 4.12C12.06 6.44 14.6 8 17.5 8c.46 0 .91-.05 1.34-.12C17.44 5.56 14.9 4 12 4c-.46 0-.91.05-1.34.12zM4.42 9.47c1.71-.97 3.03-2.55 3.66-4.44C6.37 6 5.05 7.58 4.42 9.47zM20 12c0-.78-.12-1.53-.33-2.24-.7.15-1.42.24-2.17.24-3.13 0-5.92-1.44-7.76-3.69C8.69 8.87 6.6 10.88 4 11.86c.01.04 0 .09 0 .14 0 4.41 3.59 8 8 8s8-3.59 8-8z" />
+    </SvgIcon>
+  );
+}
+
+export default DeveloperIcon;
