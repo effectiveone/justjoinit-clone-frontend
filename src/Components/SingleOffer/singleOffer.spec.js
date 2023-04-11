c284790354250681bd98887dc4f5f2a2b91da922
Konrad <konrad.gruca90@gmail.com>
2023-04-11 17:40:25 +0200

    repair small error

--- src/Components/SingleOffer/singleOffer.spec.js
+++ src/Components/SingleOffer/singleOffer.spec.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import SingleOffer from "./SingleOffer";
+import SingleOffer from "./singleOffer";
 import { DashboardContextProvider } from "../../Context/useDashboardContext";
 
 describe("SingleOffer Component", () => {
